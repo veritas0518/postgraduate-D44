a66841a7fcacb167d7ed9d5c064c008ab15555bd
TianXing Xue <76930817+veritas0518@users.noreply.github.com>
2021-06-22 17:39:42 +0800

    Exception thrown when triangle cant form triangle
    【问题描述】定义Triangle类，具有成员变量a，b和c表示三条边的长度，具有静态方法double getArea(double a, double b, double c) throws Exception计算三角形的面积，当三条边不能构成三角形时须抛出异常。
    在TestTriangle类的main()方法中，由用户输入三条边长，计算三角形的面积，并对各种异常情况进行处理。输入输出格式如样例所示，其中：红色文字为真正的程序输入，蓝色文字为输入提示。
    【样例输入1】
    1 2 3
    【样例输出1】
    Enter sides of triangle:
    1 2 3
    Not a triangle!
    
    【样例输入2】
    12 3a 28
    【样例输出2】
    Enter sides of triangle:
    12 3a 28
    Error, not all numbers!
    
    【样例输入3】
    3 4 5
    【样例输出3】
    Enter sides of triangle:
    3 4 5
    Area: 6.0

---
+++ TestTriangle1.java
@@ -0,0 +1,44 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TestTriangle1 {
+    public static void main(String[] args) {
+        Triangle1 y=new Triangle1();
+        Scanner s = new Scanner(System.in);
+        try {
+            System.out.println("Enter sides of triangle:");
+             y.a = s.nextDouble();
+             y.b = s.nextDouble();
+             y.c = s.nextDouble();
+            System.out.printf("Area: %.4f*",y.getArea(y.a,y.b,y.c) );
+        } catch (InputMismatchException e) {
+            System.out.println("Error, not all numbers!");
+        } catch (Exception e) {
+            System.out.println("Not a triangle!");
+        }
+    }
+}
+class Triangle1 {
+    double a;
+    double b;
+    double c;
+
+
+    public static double getArea(double a, double b, double c) throws Exception {
+        double Area = 0 ,p;
+        if (a + b > c && a + c > b && c + b > a) {
+            p = (a + b + c) / (2.0);
+             Area = Math.sqrt(Math.abs(p * (p - a) * (p - b) * (p - c)));
+        }
+else{
+    throw new Exception();
+        }
+return Area;
+    }
+}
+
+
+
+
+
+
